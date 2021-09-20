e20cb4bfed50fc82d890188dbdc645dc79026805
Szymon Burak <sajmon0031@gmail.com>
2021-09-20 14:37:28 +0200

    Fix bug

--- server/databaseConnection.js
+++ server/databaseConnection.js
@@ -4,7 +4,7 @@ const con = mysql.createConnection({
     connectionLimit: 100,
     host: "18421_hideisland.skylo-pl.atthost24.pl",
     user: "18421_hideisland",
-    password: "SwinkaPeppa-31",
+    password: "***** *** (Sw...)",
     database: "18421_hideisland"
 });
 
